5dba2c91e5ad9111b812da90cbdca37eb5042e30
rgc <74342662+167rgc911@users.noreply.github.com>
2024-04-04 16:58:36 +0900

    initial commit

---
+++ bm_valarrays.cpp
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  bm_valarrays.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/04/24 16:58:06
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  rgc (rgc), sessyargc.jp@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
