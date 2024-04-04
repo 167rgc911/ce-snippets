8d6bc03fd6debb1c91edf1d9961c42e61c5ee364
rgc <74342662+167rgc911@users.noreply.github.com>
2024-04-04 17:01:17 +0900

    from godbolt

--- bm_valarrays.cpp
+++ bm_valarrays.cpp
@@ -16,4 +16,91 @@
  * =====================================================================================
  */
 
+#include <algorithm>
+#include <iostream>
+#include <valarray>
+#include <vector>
+
+#include <random>
+
+std::vector<double>
+random_v(int sz)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(1.0, 99.0);
+
+    std::vector<double> sv {};
+
+    for(auto i = 0; i < sz; ++i)
+    {
+        sv.push_back(dis(gen));
+    }
+
+    return sv;
+}
+
+#include <benchmark/benchmark.h>
+
+void
+// t1(const std::valarray<double>& vad)
+t1(const std::vector<double>& vad_)
+{
+    // const std::valarray<double> vad = std::valarray<double>(vad_.data(), vad_.size());
+    const auto vad = std::valarray<double>(vad_.data(), vad_.size());
+
+    // [&vad]() {
+    //     std::for_each(std::begin(vad), std::end(vad),
+    //         [](const double v) {
+    //             // std::cout << v << ", ";
+    //         });
+    // }();
+
+    auto mean = vad.sum() / vad.size();
+
+    return;
+}
+
+void
+t2(const std::vector<double>& vad)
+{
+    // [&vad]() {
+    //     std::for_each(std::begin(vad), std::end(vad),
+    //         [](const double v) {
+    //             std::cout << v << ", ";
+    //         });
+    // }();
+
+    auto mean = std::accumulate(vad.begin(), vad.end(), 0.0) / vad.size();
+
+    return;
+}
+
+static void BM_t1(benchmark::State& state) {
+  // Perform setup here
+  const auto vad_ = random_v(256);
+//   const std::valarray<double> vad = std::valarray<double>(vad_.data(), vad_.size());
+  for (auto _ : state) {
+    // This code gets timed
+    t1(vad_);
+  }
+}
+
+static void BM_t2(benchmark::State& state) {
+  // Perform setup here
+  const auto vad = random_v(256);
+  for (auto _ : state) {
+    // This code gets timed
+    t2(vad);
+  }
+}
+
+// Register the function as a benchmark
+BENCHMARK(BM_t1);
+// BENCHMARK(BM_test_list);
+BENCHMARK(BM_t2);
+
+
+// Run the benchmark
+BENCHMARK_MAIN();
 
