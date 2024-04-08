c3166d3ea2e302c42d4475f8ece785ba0184e02f
rgc <74342662+167rgc911@users.noreply.github.com>
2024-04-08 12:28:48 +0000

    update; :sp: value semantics talk

---
+++ src/sp_value_semantics1.cpp
@@ -0,0 +1,54 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  sp_value_semantics1.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/08/24 11:49:13
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  rgc (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <vector>
+
+using object_t = int;
+
+void
+draw(const object_t& x, std::ostream& out, std::size_t position)
+{
+  out << std::string(position, ' ') << x << std::endl;
+}
+
+using document_t = std::vector<object_t>;
+
+void
+draw(const document_t& x, std::ostream& out, std::size_t position)
+{
+  out << std::string(position, ' ') << "document" << std::endl;
+  for (auto e: x) draw(e, out, position + 2);
+  out << std::string(position, ' ') << "/document" << std::endl;
+}
+
+
+int
+main()
+{
+  document_t document;
+
+  document.emplace_back(0);
+  document.emplace_back(1);
+  document.emplace_back(2);
+  document.emplace_back(3);
+
+  draw(document,std::cout, 0);
+}
---
+++ src/sp_value_semantics2.cpp
@@ -0,0 +1,63 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  sp_value_semantics2.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/08/24 11:56:29
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  rgc (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <vector>
+
+void
+draw(const int& x, std::ostream& out, std::size_t position)
+{
+  out << std::string(position, ' ') << x << std::endl;
+}
+
+class
+object_t
+{
+public:
+  object_t(const int& x) : object_(x) {}
+  friend void draw(const object_t& x, std::ostream& out, std::size_t position)
+  { draw(x.object_, out, position); }
+private:
+  int object_;
+};
+
+using document_t = std::vector<object_t>;
+
+void
+draw(const document_t& x, std::ostream& out, std::size_t position)
+{
+  out << std::string(position, ' ') << "document" << std::endl;
+  for (auto e: x) draw(e, out, position + 2);
+  out << std::string(position, ' ') << "/document" << std::endl;
+}
+
+
+int
+main()
+{
+  document_t document;
+
+  document.emplace_back(0);
+  document.emplace_back(1);
+  document.emplace_back(2);
+  document.emplace_back(3);
+
+  draw(document,std::cout, 0);
+}
